77ea9f6204a7a1843568d0c60f1126a0e5c433e6
garden627 <garden627@yahoo.co.jp>
2022-10-22 12:58:35 +0900

    test

--- backend/settings.py
+++ backend/settings.py
@@ -132,20 +132,20 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'proshop',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5433'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'proshop',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'localhost',
+#         'PORT': '5433'
+#     }
+# }
 
 # print(os.environ['DB_PASS'])
 
-# DATABASES = {'default': dj_database_url.config(default='postgres://postgres:postgres@localhost/proshop')}
+DATABASES = {'default': dj_database_url.config(default='postgres://postgres:postgres@localhost/proshop')}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
